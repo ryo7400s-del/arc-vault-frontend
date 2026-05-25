1569e6e414e83f07213f080ee8a374a1b12f61d9
com/ryo7400s-del <ryo7400.s@gmail.com>
2026-05-25 14:29:00 +0900

    fix: remove duplicate allowance in ERC20_ABI

--- pages/arb.js
+++ pages/arb.js
@@ -29,6 +29,12 @@ const CURVE_ABI = [
 ];
 
 const ERC20_ABI = [
+  { name: "allowance", type: "function", stateMutability: "view",
+    inputs: [{ name: "owner", type: "address" },{ name: "spender", type: "address" }],
+    outputs: [{ name: "", type: "uint256" }] },
+  { name: "allowance", type: "function", stateMutability: "view",
+    inputs: [{ name: "owner", type: "address" },{ name: "spender", type: "address" }],
+    outputs: [{ name: "", type: "uint256" }] },
   { name: "approve", type: "function", stateMutability: "nonpayable",
     inputs: [{ name: "spender", type: "address" },{ name: "amount", type: "uint256" }],
     outputs: [{ name: "", type: "bool" }] },
